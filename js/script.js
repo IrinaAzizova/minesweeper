0d5799c5dff7e3483ff56094d6ae71323e8f1633
Irina Azizova <devazizova@mail.ru>
2023-05-12 10:12:22 +0300

    feat: add eslint and basic layout

---
+++ js/script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {    
+
+    const toCreateCell = () => {
+        const field = document.querySelector('.field');
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        field.append(cell);
+    }
+
+    const toCreateLayout = (createBtnFunc) => {
+        const body = document.body;
+        const main = document.createElement('main');
+        main.classList.add('main');
+        main.innerHTML = `
+            <h1 class="main__title">Minesweeper</h1>
+            <p class="main__game-status">Mines on playing field: <span>10</span></p>
+            <div class="field">
+            </div>
+        `;
+        body.insertAdjacentElement('afterbegin', main);
+        for (let i = 0; i < 100; i++) {
+            createBtnFunc();
+        }
+    }
+    
+    toCreateLayout(toCreateCell);
+});
+
