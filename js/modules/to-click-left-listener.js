3dd1fcd9d373aa5f22a225a78ed230f067890874
Irina Azizova <devazizova@mail.ru>
2023-05-16 21:18:25 +0300

    feat: little changes in titels generation

--- js/modules/to-click-left-listener.js
+++ js/modules/to-click-left-listener.js
@@ -5,6 +5,8 @@ const clickLeftListener = () => {
     const timer = document.querySelector('.timer')
     const startBtn = document.querySelector('.main__btn');
 
+    let clickCounter = 0;
+
     const removeClick = () => {
         cells.forEach(cell => {
             cell.style.cursor = 'default';
@@ -13,26 +15,29 @@ const clickLeftListener = () => {
     }
 
     const listenerCallback = (event) => {
+        clickCounter++
+        console.log(clickCounter);
+        const endGame = () => {
+            removeClick();
+            timer.remove();
+            startBtn.style.display = 'block';
+        }        
+        let timerContent = timer.textContent;
         
-        const timerContent = timer.textContent;
         if (event.target.dataset.content === 'b') {
             event.target.classList.add('cell_bomb');
-            gameStatus.textContent = `You lose. Your time ${timerContent} Try again.`;
+            gameStatus.textContent = `You lose. Your time ${timerContent}. Try again.`;
             gameStatus.style.color = 'black';
             gameStatus.style.fontWeight = 'bold';
-            removeClick();
-            timer.remove();           
-            setTimeout(() => {
-                startBtn.style.display = 'block';
-            }, 3000);
+            endGame();
         } else {
             event.target.textContent = event.target.dataset.content;
             event.target.classList.add('cell_open');
             const cellsOpen = document.querySelectorAll('.cell_open');
             if (cellsOpen.length >= 90) {
-                gameStatus.textContent = 'You win. Congratulations!';
+                gameStatus.innerHTML = `You win.<br>Your time ${timerContent}.<br>Number of moves ${clickCounter}.<br>Congratulations!`;
                 gameStatus.style.color = 'darkred';
-                removeClick();
+                endGame();
             }
         }
     }
